12c868ed85cc87e3d2c41858112be3f5d4f9e9cf
J0K3RY <jokery03@hotmail.com>
2023-06-30 09:27:43 +0200

    finished first version page create character with get api

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+const allCharacterContainer = document.querySelector('.container_character_list')
 
 function getAllCharacter() {
     const getAllCharacterApi = fetch('https://character-database.becode.xyz/characters');
@@ -7,10 +7,40 @@ function getAllCharacter() {
         try {
             const allCharacter = await response.json()
             allCharacter.forEach(element => {
-                
+                console.log(element)
+                const imgBase64ToJs = 'data:image/png;base64,' + element.image;
+                const createArticle = document.createElement('article');
+                createArticle.setAttribute('class', 'character_list_card');
+
+                allCharacterContainer.append(createArticle);
+                const createFigure = document.createElement('figure');              
+                createArticle.append(createFigure);
+
+                const createImgInFigure = document.createElement('img')
+                createImgInFigure.setAttribute('src', imgBase64ToJs);
+                createFigure.append(createImgInFigure);
+
+                const createDivTextContent = document.createElement('div');
+                createDivTextContent.setAttribute('class', 'card_text_content');
+                createFigure.insertAdjacentElement('afterend', createDivTextContent);
+
+                const createNameCharacter = document.createElement('h3');
+                const nameTextNode = document.createTextNode(element.name);
+                createNameCharacter.appendChild(nameTextNode);
+                createDivTextContent.appendChild(createNameCharacter);
+
+                const createParagraph = document.createElement('p');
+                const paragraphTextNode = document.createTextNode(element.shortDescription);
+                createParagraph.appendChild(paragraphTextNode);
+                createNameCharacter.insertAdjacentElement('afterend', createParagraph);
+
+                const createBtn = document.createElement('a');
+                createBtn.setAttribute('class', 'btn');
+                createBtn.setAttribute('href', '');
+                createParagraph.insertAdjacentElement('afterend', createBtn);
             });
         } catch (error) {
-            
+            console.log('ERROR', error);
         }
     }).catch((err) => {
         console.log(err)
@@ -18,3 +48,11 @@ function getAllCharacter() {
 }
 
 getAllCharacter()
+
+const link = document.querySelectorAll('.btn');
+
+link.forEach(elem => {
+  elem.addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+});
