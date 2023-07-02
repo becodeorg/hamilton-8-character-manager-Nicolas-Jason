021b47b070e9e9538031a80b115990464f4d0c5b
J0K3RY <jokery03@hotmail.com>
2023-07-02 23:31:31 +0200

    finished character list and card for view unique character

---
+++ js/create_character.js
@@ -0,0 +1,68 @@
+const btnSubmit = document.querySelector('.btn-create-send');
+let inputFile = document.querySelector('#file-input')
+let fileNameField = document.querySelector('#file-name')
+
+inputFile.addEventListener('change', function(event) {
+    const containerFile = document.querySelector('.container_upload_file');
+    const imgPreview = document.createElement('img')
+    let uploadFileName = event.target.files[0].name;
+    let uploadFile = inputFile.files[0];
+    console.log(uploadFile)
+    const createDivImg = document.createElement('div');
+    containerFile.insertAdjacentElement('beforeend',createDivImg)
+    imgPreview.setAttribute('src','');
+    createDivImg.appendChild(imgPreview);
+    fileNameField.textContent = uploadFileName;
+
+    if (uploadFile) {
+        const imageURL = URL.createObjectURL(uploadFile);
+        imgPreview.src = imageURL;
+        imgPreview.style.display = 'block';
+        imgPreview.style.height = '80px'
+        imgPreview.style.width = '120px'
+        imgPreview.style.marginTop = '1rem'
+    } else {
+        imgPreview.src = '';
+        imgPreview.style.display = 'none';
+    }
+
+})
+
+
+btnSubmit.addEventListener('click', async function(event) {
+    event.preventDefault();
+    const image = document.querySelector('#file-input').files[0];
+    const name = document.querySelector('#characterName').value;
+    const shortDesc = document.querySelector('#shortDesc').value;
+    const completDesc = document.querySelector('#completDesc').value;
+
+    const reader = new FileReader();
+
+        reader.onload = function() {
+            const imageData = reader.result;
+            sendDataToAPI(imageData);
+        };
+
+        reader.readAsDataURL(image);
+
+    async function sendDataToAPI(imageData) {
+        try {
+            const sendDataToAPI = await fetch('https://character-database.becode.xyz/characters', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    image: imageData.split(',')[1],
+                    name: name,
+                    shortDescription: shortDesc,
+                    description: completDesc
+                })
+            });
+            const data = await sendDataToAPI.json();
+            console.table('DATA OK', data);
+    }   catch (error) {
+            console.log(error);
+        }
+    }
+})
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const allCharacterContainer = document.querySelector('.container_character_list')
+const allCharacterContainer = document.querySelector('.container_character_list');
+const getBody = document.querySelector('body');
+const getOverlay = document.querySelector('#overlay');
 
 function getAllCharacter() {
     const getAllCharacterApi = fetch('https://character-database.becode.xyz/characters');
@@ -7,7 +9,6 @@ function getAllCharacter() {
         try {
             const allCharacter = await response.json()
             allCharacter.forEach(element => {
-                console.log(element)
                 const imgBase64ToJs = 'data:image/png;base64,' + element.image;
                 const createArticle = document.createElement('article');
                 createArticle.setAttribute('class', 'character_list_card');
@@ -37,8 +38,12 @@ function getAllCharacter() {
                 const createBtn = document.createElement('a');
                 createBtn.setAttribute('class', 'btn');
                 createBtn.setAttribute('href', '');
+                createBtn.setAttribute('data-id', element.id);
                 createParagraph.insertAdjacentElement('afterend', createBtn);
+                // getIdOfCharacter(getAllBtn);
             });
+            const getAllBtn = document.querySelectorAll('.btn');
+                removeReloadBtn(getAllBtn);
         } catch (error) {
             console.log('ERROR', error);
         }
@@ -47,12 +52,69 @@ function getAllCharacter() {
     });
 }
 
-getAllCharacter()
+function getCharacterById(idCharacter) {
+    const getCharacterApi = fetch('https://character-database.becode.xyz/characters/'+ idCharacter);
 
-const link = document.querySelectorAll('.btn');
+    getCharacterApi.then(async (response) => {
+        try {
+            const responseCharacter = await response.json();
+            const name = responseCharacter.name;
+            const shortDesc = responseCharacter.shortDescription;
+            const longDesc = responseCharacter.description;
+            const image = responseCharacter.image;
+            const cardImg = document.querySelector('.card-img-overlay');
+            const imgBase64ToJs = 'data:image/png;base64,' + image;
+            cardImg.src = imgBase64ToJs;
+            const cardName = document.querySelector('.card-name-overlay');
+            cardName.textContent = name;
+            const cardShortDesc = document.querySelector('.short-desc');
+            cardShortDesc.textContent = shortDesc;
+            const cardDescription = document.querySelector('.description');
+            cardDescription.textContent = longDesc;
 
-link.forEach(elem => {
-  elem.addEventListener('click', function(event) {
-    event.preventDefault();
-  });
+        } catch (error) {
+            console.log(error)
+        }
+    })
+}
+
+function removeReloadBtn(btn) {
+    const clickHandler = function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('data-id');
+        on();
+        getCharacterById(targetId);
+    };
+
+    btn.forEach(elem => {
+        elem.addEventListener('click', clickHandler);
+    });
+}
+
+const articleElement = document.querySelector('#overlay article');
+
+articleElement.addEventListener('click', function(event) {
+    event.stopPropagation();
 });
+
+
+getOverlay.addEventListener('click', function(event) {
+    off()
+})
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+const btnEdit = document.querySelector('.btn-edit');
+
+btnEdit.addEventListener('click', function() {
+
+})
+
+getAllCharacter()
